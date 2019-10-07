f0de4e0aea5164d823533b66ae9c7ed5a22d358e
sb <sb@qq.com>
2019-10-07 20:21:41 +0800

    第一次添加

---
+++ liuchanguse.vue
@@ -0,0 +1,65 @@
+<template>
+<div id='packed'>
+    <button @click='msgClick'>点我啊</button>
+  <h3>招逸枫</h3>
+
+  <el-button type="primary" plain disabled>主要按钮</el-button>
+  <br>
+  <br>
+  <br>
+  <br>
+  <el-upload
+  class="upload-demo"
+  action="https://jsonplaceholder.typicode.com/posts/"
+  :on-preview="handlePreview"
+  :on-remove="handleRemove"
+  :before-remove="beforeRemove"
+  multiple
+  :limit="3"
+  :on-exceed="handleExceed"
+  :file-list="fileList">
+  <el-button size="small" type="primary">点击上传</el-button>
+  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+
+ 
+</el-upload>
+
+ <el-switch
+  v-model="value"
+  active-color="#13ce66"
+  inactive-color="#ff4949">
+</el-switch>
+  </div>
+</template>
+
+<script>
+export default {
+ data() {
+      return {
+        fileList: [],
+        value:true
+      };
+    },
+    methods: {
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePreview(file) {
+        console.log(file);
+      },
+      handleExceed(files, fileList) {
+        this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+      },
+      beforeRemove(file, fileList) {
+        return this.$confirm(`确定移除 ${ file.name }？`);
+      },
+      msgClick(){
+          alert('说点咱就点')
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
